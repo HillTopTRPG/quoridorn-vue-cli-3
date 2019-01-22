ed53c0100891dd6ddb0ec6e91b04b937717b7aa9
HillTop <quoridorn@gmail.com>
2019-01-23 07:31:49 +0900

    ver.1.0.0b4 BGMFileComponent.vueのTypeScript化

--- src/components/music/component/BGMFileComponent.vue
+++ src/components/music/component/BGMFileComponent.vue
@@ -19,80 +19,77 @@
   ></BGMCoreComponent>
 </template>
 
-<script>
-import { mapActions } from "vuex";
-import BGMCoreComponent from "./BGMCoreComponent";
+<script lang="ts">
+import BGMCoreComponent from "./BGMCoreComponent.vue";
+import { Component, Prop, Vue } from "vue-property-decorator";
 
-export default {
+@Component<BGMFileComponent>({
   name: "bgmFileComponent",
-  props: {
-    tag: { type: String, required: true },
-    isLoop: { type: Boolean, required: true },
-    title: { type: String, required: true },
-    initVolume: { type: Number, required: true },
-    url: { type: String, required: true },
-    startSecond: { type: Number, required: true },
-    endSecond: { type: Number, required: true }
-  },
   components: {
-    BGMCoreComponent: BGMCoreComponent
-  },
-  data() {
-    return {
-      jukeboxAudio: null
-    };
-  },
-  destroyed() {
-    this.destroyed();
-  },
-  methods: {
-    ...mapActions([]),
-    mounted() {
-      this.jukeboxAudio = new Audio();
-      this.jukeboxAudio.autoplay = true;
-      this.jukeboxAudio.loop = this.isLoop;
-      this.jukeboxAudio.addEventListener("timeupdate", this.timeUpdate);
-      this.jukeboxAudio.addEventListener("play", () => {
-        if (!this.jukeboxAudio) return;
-        this.$refs.core.setDuration(this.jukeboxAudio.duration);
-      });
-      if (this.url.startsWith("..")) {
-        import(this.url).then(module => {
-          this.jukeboxAudio.src = module;
-        });
-      } else {
-        this.jukeboxAudio.src = this.url;
-      }
-    },
-    destroyed() {
-      this.jukeboxAudio = null;
-    },
-    mute(mute) {
-      this.jukeboxAudio.muted = mute;
-    },
-    volume(volume) {
-      this.jukeboxAudio.volume = volume;
-    },
-    play() {
-      this.jukeboxAudio.play();
-    },
-    pause() {
-      this.jukeboxAudio.pause();
-    },
-    seekTo(time) {
-      this.jukeboxAudio.currentTime = time;
-    },
-    end() {
-      this.$emit("end");
-    },
-    timeUpdate() {
+    BGMCoreComponent
+  }
+})
+export default class BGMFileComponent extends Vue {
+  @Prop({ required: true })
+  tag!: string;
+  @Prop({ required: true })
+  isLoop!: boolean;
+  @Prop({ required: true })
+  title!: string;
+  @Prop({ required: true })
+  initVolume!: number;
+  @Prop({ required: true })
+  url!: string;
+  @Prop({ required: true })
+  startSecond!: number;
+  @Prop({ required: true })
+  endSecond!: number;
+
+  private jukeboxAudio: any = null;
+
+  destroyed(): void {
+    this.jukeboxAudio = null;
+  }
+  mounted(this: any): void {
+    this.jukeboxAudio = new Audio();
+    this.jukeboxAudio.autoplay = true;
+    this.jukeboxAudio.loop = this.isLoop;
+    this.jukeboxAudio.addEventListener("timeupdate", this.timeUpdate);
+    this.jukeboxAudio.addEventListener("play", () => {
       if (!this.jukeboxAudio) return;
-      this.$refs.core.timeUpdate(this.jukeboxAudio.currentTime);
+      if (this.$refs.core instanceof BGMCoreComponent) {
+        this.$refs.core.setDuration(this.jukeboxAudio.duration);
+      }
+    });
+    if (this.url.startsWith("..")) {
+      import(this.url).then(module => {
+        this.jukeboxAudio.src = module;
+      });
+    } else {
+      this.jukeboxAudio.src = this.url;
     }
   }
-};
+  mute(mute: boolean): void {
+    this.jukeboxAudio.muted = mute;
+  }
+  volume(volume: number): void {
+    this.jukeboxAudio.volume = volume;
+  }
+  play(): void {
+    this.jukeboxAudio.play().then();
+  }
+  pause(): void {
+    this.jukeboxAudio.pause();
+  }
+  seekTo(time: number): void {
+    this.jukeboxAudio.currentTime = time;
+  }
+  end(): void {
+    this.$emit("end");
+  }
+  timeUpdate(this: any): void {
+    if (!this.jukeboxAudio) return;
+    this.$refs.core.timeUpdate(this.jukeboxAudio.currentTime);
+  }
+}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-</style>
